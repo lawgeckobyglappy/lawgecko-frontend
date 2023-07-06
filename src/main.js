e01642e03785f855a2ee69683a0f6b5c010b56b5
mankaa <mankaache10@gmail.com>
2023-07-06 20:37:54 +0100

    registered some files globally

--- src/main.js
+++ src/main.js
@@ -4,4 +4,14 @@ import router from './router'
 import store from './store'
 import './assets/tailwind.css'
 
-createApp(App).use(store).use(router).mount('#app')
+//components
+import TheButton from "@/components/utils/TheButton.vue"
+import TheNav from "@/components/Layouts/TheNav.vue"
+import TheFooter from "@/components/Layouts/TheFooter.vue"
+
+const app = createApp(App)
+
+app.use(store).use(router).mount('#app')
+    .component("TheButton", TheButton)
+    .component("TheNav", TheNav)
+    .component("TheFooter", TheFooter)
