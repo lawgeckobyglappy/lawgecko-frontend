0ba8bd9a6164508c97c97bcc862a36e8c17c6317
mankaa <mankaache10@gmail.com>
2023-07-16 16:30:41 +0100

    added internationalization for en and fr

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './assets/tailwind.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/tailwind.css";
 
 //components
-import TheButton from "@/components/buttons/TheButton.vue"
-import NavBar from "@/components/layouts/NavBar.vue"
-import TheFooter from "@/components/layouts/TheFooter.vue"
+import TheButton from "@/components/buttons/TheButton.vue";
+import NavBar from "@/components/layouts/NavBar.vue";
+import TheFooter from "@/components/layouts/TheFooter.vue";
+import i18n from "./i18n";
 
-const app = createApp(App)
+const app = createApp(App);
+app.use(i18n);
 
-app
-  .use(store)
-  .use(router)
-  .mount("#app")
-  .component("TheButton", TheButton)
-  .component("NavBar", NavBar)
-  .component("TheFooter", TheFooter);
+
+
+app.use(store);
+app.use(router);
+app.mount("#app");
+app.component("TheButton", TheButton);
+app.component("NavBar", NavBar);
+app.component("TheFooter", TheFooter);
