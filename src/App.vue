287f4559ad36b1ceb6c54eb0cab66e2f1dc58f8a
mankaa <mankaache10@gmail.com>
2023-07-16 16:30:41 +0100

    added internationalization for en and fr

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  
+  <language-switcher/>
   <router-view/>
 </template>
+
+<script>
+import LanguageSwitcher from './components/LanguageSwitcher.vue'
+
+    export default {
+        components: {
+          LanguageSwitcher,
+        }
+    }
+</script>
 
 <style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <template>
-  <h1 class="text-center mt-10 uppercase font-bold text-3xl text-green-700">The Home page</h1>
+  <h1 class="text-center mt-10 uppercase font-bold text-3xl text-green-700">
+    The Home page
+    <p>{{ $t('message') }}</p>
+    <p>{{ $t('nav.features') }}</p>
+
+    <router-link to="/about">About</router-link>
+  </h1>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
