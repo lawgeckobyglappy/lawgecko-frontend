def0dd22051782a8d1fc493fcafedb3934361ff2
mankaa <mankaache10@gmail.com>
2023-07-16 16:34:59 +0100

    some modifications

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,7 @@
 <template>
   <h1 class="text-center mt-10 uppercase font-bold text-3xl text-green-700">
-    The Home page
-    <p>{{ $t('message') }}</p>
-    <p>{{ $t('nav.features') }}</p>
-
-    <router-link to="/about">About</router-link>
+   <h1>{{ $t('message') }}</h1> 
+ 
   </h1>
 </template>
 
