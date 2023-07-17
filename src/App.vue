d1f5b2fd7db973dfa79b56ca93b142dc3e12c5ec
mankaa <mankaache10@gmail.com>
2023-07-17 05:48:05 +0100

    added font-awesome library for icons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
 <template>
-  <language-switcher/>
   <router-view/>
 </template>
 
-<script>
-import LanguageSwitcher from './components/LanguageSwitcher.vue'
 
-    export default {
-        components: {
-          LanguageSwitcher,
-        }
-    }
-</script>
 
 <style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <h1 class="text-center mt-10 uppercase font-bold text-3xl text-green-700">
    <h1>{{ $t('message') }}</h1> 
+   <fa-icon icon="user"/>
+   <fa-icon :icon="['fab','font-awesome']"/>
  
   </h1>
 </template>
