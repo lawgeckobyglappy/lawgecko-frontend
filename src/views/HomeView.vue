f5745220a640b4debc26d224698e26f906dd7d60
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-07-20 17:22:18 +0100

    nav-bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,18 @@
 <template>
-  <h1 class="text-center mt-10 uppercase font-bold text-3xl text-green-700">
-   <h1>{{ $t('message') }}</h1> 
+  <TheHeader />
+  <!-- <h1 class="text-center mt-10 uppercase font-bold text-3xl text-green-700">
+   <h1>{{ $t('message') }}</h1>
    <fa-icon icon="user"/>
    <fa-icon :icon="['fab','font-awesome']"/>
-   <LanguageSwitcher />
-  </h1>
+  </h1> -->
 </template>
 
 <script>
-import LanguageSwitcher from '@/components/LanguageSwitcher.vue';
+import TheHeader from '@/components/TheHeader.vue';
+
 export default {
   components:{
-    LanguageSwitcher
+    TheHeader
   }
 };
 </script>
