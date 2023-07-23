b75ed228a5ca34d1b3cb4154945782c65962de44
GlappyInc <136288177+GlappyInc@users.noreply.github.com>
2023-07-23 03:18:19 +0100

    Revert "Added the landing Page section"

--- src/main.js
+++ src/main.js
@@ -4,19 +4,6 @@ import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
 
-import { Row, Column, Hidden } from 'vue-grid-responsive';
-
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-
 //components
 import TheButton from "@/components/buttons/TheButton.vue";
 import NavBar from "@/components/layouts/NavBar.vue";
@@ -24,7 +11,7 @@ import TheFooter from "@/components/layouts/TheFooter.vue";
 import i18n from "./i18n";
 
 /* import the fontawesome core */
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { library } from '@fortawesome/fontawesome-svg-core'
 
 //font awesome
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -33,24 +20,19 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
-import './assets/tailwind.css'
 
 //add free icon styles to SVG core
-library.add(fas, far, fab);
+library.add(fas,far,fab)
 
 const app = createApp(App);
 app.use(i18n);
 
+
+
 app.use(store);
 app.use(router);
-app.use(vuetify); // vuetify
 app.mount("#app");
 app.component("TheButton", TheButton);
-app.component("fa-icon", FontAwesomeIcon);
 app.component("NavBar", NavBar);
 app.component("TheFooter", TheFooter);
-
-//grid responsive
-app.component("TheRow", Row);
-app.component("TheColumn", Column);
-app.component("TheHidden", Hidden);
+app.component("fa-icon", FontAwesomeIcon)
