28cb4498fdce5ccfcd0459d0e288986624d2203d
GlappyInc <136288177+GlappyInc@users.noreply.github.com>
2023-07-23 03:18:19 +0100

    Revert "Added the landing Page section"

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+
+
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-];
+ 
+      {
+        path: "/",
+        name: "home",
+        component: HomeView,
+      },
+    ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
