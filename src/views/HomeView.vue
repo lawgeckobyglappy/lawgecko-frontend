63e57cc23284a4354405c99aa07ae6d1e3570f1f
VenusV-prog <tadonkevenus@gmail.com>
2023-07-26 23:36:54 +0100

    modified a translation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -74,7 +74,7 @@
       </section>
       
 
-      <section class= "md:page-margin w-full justify-center items-center page-lineheight">
+      <section class= "md:page-margin w-full justify-center items-center page-lineheight mb-16">
         <TheRow container :gutter="80" class="p-5 gap-">
           <TheColumn :xs="12" :md="6" :lg="6" class=" pa-6">
             <div class="flex justify-center items-center">
