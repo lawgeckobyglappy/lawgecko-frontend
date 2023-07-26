bb8c449c123a6b84bcec457b1e6f4338bf171c60
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-07-26 14:06:15 +0100

    validation in progress

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -22,36 +22,39 @@
                 </div>
             </div>
             <div class="form">
-                <form>
+                <form @submit.prevent="submit">
                     <div class="name">
-                        <div>
+                        <div :class="{ 'error': form.firstNameError }">
                             <label>
                                 First Name
                             </label>
-                            <input />
+                            <input v-model="form.firstName" />
                         </div>
-                        <div>
+                        <div :class="{ 'error': form.lastNameError }">
                             <label>
                                 Last Name
                             </label>
-                            <input/>
+                            <input v-model="form.lastName" />
                         </div>
                     </div>
-                    <div>
+                    <div :class="{ 'error': form.usernameError }">
                         <label>
                             Username
                         </label>
-                        <input />
+                        <p v-if="usernameExists" class="text-red-500 text-xs">* Username Already Exists</p>
+                        <input v-model="form.username" />
                     </div>
-                    <div>
+                    <div :class="{ 'error': form.emailAddressError }">
                         <label>
                             Email Address
                         </label>
-                        <input />
+                        <p v-if="emailExists" class="text-red-500 text-xs">* Email Already Exists</p>
+                        <input v-model="form.emailAddress" />
                     </div>
-                    <div class="policy-agreement">
-                        <input type="checkbox" id="checkboxInput"/>
+                    <div class="policy-agreement" :class="{ 'error': !form.policySigned }">
+                        <input type="checkbox" id="checkboxInput" v-model="form.policySigned"/>
                         <label for="termsCheckbox">
+                            <p class="text-xs text-red-500" v-if="form.policySigned === false">* Checkbox not checked</p>
                             By signing up, you agree to our Terms of Service and Privacy Policy,
                             including Cookie Use
                         </label>
@@ -64,6 +67,62 @@
         </div>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            form : {
+                firstName: "",
+                lastName: "",
+                username: "",
+                emailAddress: "",
+                policySigned: null,
+                firstNameError: "",
+                lastNameError: "",
+                usernameError: "",
+                emailAddressError: "",
+
+            }, 
+            usernameExists: false,
+            emailExists: false,
+            emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        }
+    },
+
+    methods: {
+        async submit(){
+            this.validateUserData()
+            if(this.isAllValidated){
+                console.log(this.form.emailAddress)
+                this.resetForm()
+            }
+        },
+
+        validateUserData(){
+            this.form.firstNameError = this.form.firstName === "";
+            this.form.lastNameError = this.form.lastName === "";
+            this.form.usernameError = this.form.username === "";
+            this.form.policySigned = this.form.policySigned === true;
+            this.form.emailAddressError = this.form.emailAddress === "" || !this.emailRegex.test(this.form.emailAddress)
+        },
+
+        resetForm(){
+            this.form.firstName = "",
+            this.form.lastName = "",
+            this.form.username = "",
+            this.form.policySigned = null
+            this.form.emailAddress = ""
+        }
+    },
+
+    computed: {
+        isAllValidated(){
+            return !this.form.firstNameError && !this.form.lastNameError && !this.form.usernameError
+            && !this.form.emailAddressError && this.form.policySigned
+        }
+    }
+}
+</script>
 <style scoped>
 .signup-container{
     /* max-width: 600px; */
@@ -164,6 +223,9 @@ input {
     width: 20px;
     height: 20px;
     margin-right: 10px;
+}
+.error input{
+    border-color: red;
 }
 
 @media (max-width: 600px) {
