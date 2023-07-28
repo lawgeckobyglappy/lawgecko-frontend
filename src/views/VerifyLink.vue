99eb3e43762e0d520c69b355b484be8df02b59a9
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-07-28 13:27:25 +0100

    verify-link working

---
+++ src/views/VerifyLink.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Verifying Link...</h1>
+</template>
+<script>
+import axios from 'axios';
+import { API_URL } from '@/constant';
+export default{
+    created(){
+        const id = this.$route.query.id
+        this.verifyLoginLink(id)
+    },
+
+    methods: {
+        async verifyLoginLink(linkId){
+            try{
+                const response = await axios.post(`${API_URL}/auth/verify-login-link`, {id: linkId})
+                console.log(response)
+            } catch(error){
+                console.log(error)
+            }
+        }
+    }
+}
+</script>
