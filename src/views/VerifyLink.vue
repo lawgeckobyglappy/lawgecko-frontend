b361bb4af52d1b04ac714d1590ccc50deb4e2a2b
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-08-03 05:05:54 +0100

    signup with google

--- src/views/VerifyLink.vue
+++ src/views/VerifyLink.vue
@@ -1,18 +1,28 @@
 <template>
     <h1>Verifying Link...</h1>
+    <!-- <GoogleLogin :callback="callback"/> -->
 </template>
 <script>
+// import { GoogleLogin } from 'vue3-google-login';
+// import { decodeCredential } from 'vue3-google-login'
 
 export default{
-    created(){
-        const id = this.$route.query.id
-        this.verifyLoginLink(id)
+    created() {
+        const id = this.$route.query.id;
+        this.verifyLoginLink(id);
     },
-
     methods: {
-        async verifyLoginLink(linkId){
-            await this.$store.dispatch('verifyLoginLink', linkId)
-        }, 
-    }
+        async verifyLoginLink(linkId) {
+            await this.$store.dispatch('verifyLoginLink', linkId);
+        },
+
+        // callback(response){
+        //     // This callback will be triggered when the user selects or login to
+        //     // his Google account from the popup
+        //     // const userData = decodeCredential(response.credential)
+        //     // console.log("Handle the userData", userData)
+        // }
+    },
+    // components: { GoogleLogin }
 }
 </script>
