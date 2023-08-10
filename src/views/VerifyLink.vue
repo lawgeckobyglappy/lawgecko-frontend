f4de8de7ff5dbfd7f7b98465b540fbc43f119f0e
Deji Martins <dejimartins99@gmail.com>
2023-08-11 00:56:09 +0100

    sign in with google working

--- src/views/VerifyLink.vue
+++ src/views/VerifyLink.vue
@@ -1,10 +1,7 @@
 <template>
     <h1>Verifying Link...</h1>
-    <!-- <GoogleLogin :callback="callback"/> -->
 </template>
 <script>
-// import { GoogleLogin } from 'vue3-google-login';
-// import { decodeCredential } from 'vue3-google-login'
 
 export default{
     created() {
@@ -15,14 +12,6 @@ export default{
         async verifyLoginLink(linkId) {
             await this.$store.dispatch('verifyLoginLink', linkId);
         },
-
-        // callback(response){
-        //     // This callback will be triggered when the user selects or login to
-        //     // his Google account from the popup
-        //     // const userData = decodeCredential(response.credential)
-        //     // console.log("Handle the userData", userData)
-        // }
     },
-    // components: { GoogleLogin }
 }
 </script>
