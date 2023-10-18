5230a079f568e7a6e62d8d65bb86f71f8e294a75
KellyAvre <kellyavre@gmail.com>
2023-10-18 19:49:59 +0100

    implement responsiveness on signup and sign in

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -3,13 +3,13 @@
         <div class="signin-inner-container">
 
             <div class="image-container">
-                <div class="image rounded-top"></div> <!-- First div with rounded top -->
-                <div class="image"></div> <!-- Second div (no rounded borders) -->
-                <div class="image rounded-bottom"></div> <!-- Third div with rounded bottom -->
+                <img src="../assets/images/auth.png" class="w-full" />
             </div>
 
         <div class="form-content">
-            <h1 class="mb-8 mt-1 text-heading-text font-bold text-2xl">Lawgeck<span class="text-orange-400"><span class="big-o">o</span></span></h1>
+            <div class="logo">
+                <img src="/logo.svg" alt="lawgecko logo" class="w-40 logo mb-50" />
+            </div>
             <p class="mb-10 text-heading-text font-bold text-2xl">{{$t('signin.paragraph')}}</p> 
             <div class="form">
                 <form @submit.prevent="submit">
@@ -39,8 +39,9 @@
                     <hr/>
                 </div>
             </div>
-            <button class="google-button" @click="registerUsingGoogle">
-                Register Using Google
+            <button id="google-btn" class="bg-btn-blue mt-8 mb-5 text-white" @click="googleAuth">
+                <img src="../assets/images/google.png"/>
+                Sign up with Google
             </button>
             <div class="signin-link">
                 <p class="text-center">Don't Have an Account? <a href="SignUp">Sign Up</a></p>
@@ -131,135 +132,96 @@ export default {
 }
 </script>
 <style scoped>
-.signin-container{
-    background-image: url("../assets/images/backgroundImage.png");
-    background-size: cover;
-    background-position: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
+.logo {
+  margin: 15px auto 10px;
 }
-.signin-inner-container{
-    display: flex;
-    align-items: center;
-    width: 100%;
+.signin-container {
+  background-image: url("../assets/images/backgroundImage.png");
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+.signin-inner-container {
+  display: flex;
+  align-items: center;
+  width: 100%;
 }
 .form-content {
   display: flex;
   flex-direction: column;
-  margin: auto;
+  width: 100%;
+  margin: 0 auto;
+  margin-left: 150px;
+  margin-right: 150px;
 }
-/* Images*/
+/* Images */
 .image-container {
   display: flex;
   flex-direction: column;
-  margin-right: 20px;
   margin-left: 10px;
+  width: 50%;
 }
-
-.image {
-  width: 300px;
-  height: 180px;
-  background-color: #ccc;
-  margin-bottom: 1px;
-}
-
-.rounded-top {
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-}
-
-.rounded-bottom {
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-}
-.signin-inner-container h1 {
-    text-align: center;
-    margin-bottom: 3vh;
-}
-.or-demarcation{
-    display: grid;
-    grid-template-columns: 1fr auto 1fr;
-    grid-gap: 10px;
+.or-demarcation {
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  grid-gap: 10px;
 }
 .hr {
-    height: 1.5px;
-    margin-top: 12px;
-    margin-bottom: 20px;
-    background-color: #cbcaca;
+  height: 1.5px;
+  margin-top: 12px;
+  margin-bottom: 20px;
+  background-color: #cbcaca;
 }
 .or-text {
-    margin: 0 10px;
-    font-weight: 600;
-    color: #6E6E6E;
+  margin: 0 10px;
+  font-weight: 600;
+  color: #6E6E6E;
 }
-.form input{
-    width: 100%;
-    padding: 10px;
-    margin-bottom: 15px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
+.form input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
 }
-.form button{
-    width: 100%;
-    height: 47px;
-    padding: 10px;
-    margin-bottom: 15px;
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.form button {
+  width: 100%;
+  height: 47px;
+  padding: 10px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.auth {
-    display: flex;
-    justify-content: space-between;
-    max-width: 150px;
-    margin: 10px auto;
+.error input {
+  border-color: red;
 }
-.auth button {
-    border: 1px solid gray;
-    border-radius: 5px;
-    display: flex;
-    flex-direction: row;
+.signin-link {
+  font-weight: 600;
+  color: #6E6E6E;
 }
-.auth button img {
-    height: 30px;
-    width: 30px;
-    padding: 5px;
+button {
+  width: 100%;
+  height: 47px;
+  padding: 10px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.error input{
-    border-color: red;
-}
-.big-o {
-    font-size: 1.2em;
-  }
-  .google-button {
-    background-color: rgb(49, 69, 179);
-    color: white;
-    border: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 47px;
-    padding: 10px;
-    border-radius: 5px;
-    margin-bottom: 15px;
-    cursor: pointer;
-  }
-  .signin-link{
-    font-weight: 600;
-    color: #6E6E6E;
-  }
-  @media (max-width: 768px) {
+@media (max-width: 768px) {
   .image-container {
     display: none;
   }
   .form-content {
     display: flex;
     flex-direction: column;
-    margin-top: -200px; 
-    }
+    margin-top: -200px;
+    margin: auto;
+  }
 }
 </style>
