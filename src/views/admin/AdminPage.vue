f082a8bc368fb78095b6a26658958738638640d0
Deji Martins <dejimartins99@gmail.com>
2023-10-26 13:40:56 +0100

    admin page

---
+++ src/views/admin/AdminPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <SideBar />
+    </div>
+    <div>
+        <h1>Site Admins</h1>
+        <TheButton text="Add Sub Admin" />
+        <div>
+            <p>This is the Admin Page</p>
+        </div>
+    </div>
+</template>
+<script>
+import TheButton from '@/components/buttons/TheButton.vue';
+import SideBar from '@/components/layouts/SideBar.vue';
+
+export default {
+    components: {
+        TheButton,
+        SideBar
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/admin/SubAdmin.vue
+++ src/views/admin/SubAdmin.vue
@@ -1,18 +1,10 @@
 <template>
-    <main>
-        <h1>Site Admins</h1>
-        <TheButton text="Add Sub Admin" />
-        <div>
-            <p>This is the Admin Page</p>
-        </div>
-    </main>
+    <div>
+        
+    </div>
 </template>
-<script>
-import TheButton from '@/components/buttons/TheButton.vue';
 
-export default {
-    components: {
-        TheButton
-    }
-}
+ 
+<script>
+
 </script>
